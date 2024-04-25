dd76ec6fecc856f7b567e1d72932664c783a508e
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-25 19:54:33 +0800

    4/25 Struct Capital Export1-7

--- main.go
+++ main.go
@@ -35,125 +35,18 @@ type Item struct {
 	Ca5N            string `json:"ca5N" bson:"ca5N"`
 	Ca6N            string `json:"ca6N" bson:"ca6N"`
 	Ca7N            string `json:"ca7N" bson:"ca7N"`
-	Cna1            string
-	Cna2            string
-	Cna3            string
-	Cna4            string
-	Cna5            string
-	Cna6            string
-	Cna7            string
-	Cna9            string
-	Cna10           string
-	CnewestRevMonth string
-	CluX            string
-	CnluXMoM        string
-
-	//CProfitN []string
-	//CProfit  []string
-	Cb1N  string
-	Cb2N  string
-	Cb3N  string
-	Cb4N  string
-	Cb5N  string
-	Cb6N  string
-	Cb7N  string
-	Cb8N  string
-	Cb1   string
-	Cb2   string
-	Cb3   string
-	Cb4   string
-	Cb5   string
-	Cb6   string
-	Cb7   string
-	Cb8   string
-	Cb9   string
-	Cb10  string
-	Cb10P string
-
-	//CInvTON     []string
-	//CInvTO      []string
-	Ce1N        string
-	Ce2N        string
-	Ce3N        string
-	Ce4N        string
-	Ce5N        string
-	Ce6N        string
-	Ce7N        string
-	Ce8N        string
-	Ce1         string
-	Ce2         string
-	Ce3         string
-	Ce4         string
-	Ce5         string
-	Ce6         string
-	Ce7         string
-	Ce8         string
-	CnewestFinQ string
-
-	//CNetIncomeN []string
-	//CNetIncome  []string
-	Cc1N  string
-	Cc2N  string
-	Cc3N  string
-	Cc4N  string
-	Cc5N  string
-	Cc6N  string
-	Cc7N  string
-	Cc8N  string
-	Cc1   string
-	Cc2   string
-	Cc3   string
-	Cc4   string
-	Cc5   string
-	Cc6   string
-	Cc7   string
-	Cc8   string
-	Cc9   string
-	Cc10  string
-	Cc11  string
-	Cpc9  string
-	Cpc10 string
-	Cpc11 string
-
-	//CEPSN []string
-	//CEPS  []string
-	CD1N string
-	CD2N string
-	CD3N string
-	CD4N string
-	CD5N string
-	CD6N string
-	CD7N string
-	CD8N string
-	CD1  string
-	CD2  string
-	CD3  string
-	CD4  string
-	CD5  string
-	CD6  string
-	CD7  string
-	CD8  string
-
-	//CCashFlowN []string
-	//CCashFlow  []string
-	Cf1N string
-	Cf2N string
-	Cf3N string
-	Cf4N string
-	Cf5N string
-	Cf6N string
-	Cf7N string
-	Cf8N string
-	Cf1  string
-	Cf2  string
-	Cf3  string
-	Cf4  string
-	Cf5  string
-	Cf6  string
-	Cf7  string
-	Cf8  string
-	Cf9  string
-	Cf10 string
+	Cna1            string `json:"cna1" bson:"cna1"`
+	Cna2            string `json:"cna2" bson:"cna2"`
+	Cna3            string `json:"cna3" bson:"cna3"`
+	Cna4            string `json:"cna4" bson:"cna4"`
+	Cna5            string `json:"cna5" bson:"cna5"`
+	Cna6            string `json:"cna6" bson:"cna6"`
+	Cna7            string `json:"cna7" bson:"cna7"`
+	Cna9            string `json:"cna9" bson:"cna9"`
+	Cna10           string `json:"cna10" bson:"cna10"`
+	CnewestRevMonth string `json:"cnewestRevMonth" bson:"cnewestRevMonth"`
+	CluX            string `json:"cluX" bson:"cluX"`
+	CnluXMoM        string `json:"cnluXMoM" bson:"cnluXMoM"`
 }
 
 var collection *mongo.Collection
