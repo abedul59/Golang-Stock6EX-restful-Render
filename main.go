0310469284da9361a3ba34d87e60ede87a1680f9
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-25 18:27:19 +0800

    4/25 Struct Capital Export1

--- main.go
+++ main.go
@@ -12,155 +12,142 @@ import (
 )
 
 type Item struct {
-	StockID      string `json:"cStockID" bson:"cStockID"`
-	StockName    string `json:"cStockName" bson:"cStockName"`
-	NewestSeason string `json:"cNewestSeason" bson:"cNewestSeason"`
-	NewestRev    string `json:"cNewestRev" bson:"cNewestRev"`
-
-	Sign1        string `json:"cSign1" bson:"cSign1"`
-	Sign2        string `json:"cSign2" bson:"cSign2"`
-	Sign3        string `json:"cSign3" bson:"cSign3"`
-	Sign4        string `json:"cSign4" bson:"cSign4"`
-	Sign5        string `json:"cSign5" bson:"cSign5"`
-	Sign6        string `json:"cSign6" bson:"cSign6"`
-	AverageScore string `json:"cAverageScore" bson:"cAverageScore"`
-	LossGain     string `json:"cLossGain" bson:"cLossGain"`
-	CreateDate   string `json:"CreateDate" bson:"CreateDate"`
-}
-
-type Item1 struct {
-	StockID string `json:"cStockID" bson:"cStockID"`
-	RevN    string `json:"cRevN" bson:"cRevN"`
-	Rev     string `json:"cRev" bson:"cRev"`
-	a1N     string `json:"ca1N" bson:"ca1N"`
-	ca2N    string `json:"ca2N" bson:"ca2N"`
-	ca3N    string `json:"ca3N" bson:"ca3N"`
-	ca4N    string `json:"ca4N" bson:"ca4N"`
-	ca5N    string `json:"ca5N" bson:"ca5N"`
-	ca6N    string `json:"ca6N" bson:"ca6N"`
-	ca7N    string `json:"ca7N" bson:"ca7N"`
-}
-
-type Item2 struct {
-	cna1              string
-	cna2              string
-	cna3              string
-	cna4              string
-	cna5              string
-	cna6              string
-	cna7              string
-	cna9              string
-	cna10             string
-	cnewest_Rev_month string
-	cluX              string
-	cnluX_MoM         string
-
-	cProfitN string
-	cProfit  string
-	cb1N     string
-	cb2N     string
-	cb3N     string
-	cb4N     string
-	cb5N     string
-	cb6N     string
-	cb7N     string
-	cb8N     string
-
-	cb1           string
-	cb2           string
-	cb3           string
-	cb4           string
-	cb5           string
-	cb6           string
-	cb7           string
-	cb8           string
-	cb9           string
-	cb10          string
-	cb10p         string
-	cInvTON       string
-	cInvTO        string
-	ce1N          string
-	ce2N          string
-	ce3N          string
-	ce4N          string
-	ce5N          string
-	ce6N          string
-	ce7N          string
-	ce8N          string
-	ce1           string
-	ce2           string
-	ce3           string
-	ce4           string
-	ce5           string
-	ce6           string
-	ce7           string
-	ce8           string
-	cnewest_Fin_Q string
-
-	cNetIncomeN string
-	cNetIncome  string
-	cc1N        string
-	cc2N        string
-	cc3N        string
-	cc4N        string
-	cc5N        string
-	cc6N        string
-	cc7N        string
-	cc8N        string
-	cc1         string
-	cc2         string
-	cc3         string
-	cc4         string
-	cc5         string
-	cc6         string
-	cc7         string
-	cc8         string
-	cc9         string
-	cc10        string
-	cc11        string
-	cpc9        string
-	cpc10       string
-	cpc11       string
-
-	cEPSN string
-	cEPS  string
-	cd1N  string
-	cd2N  string
-	cd3N  string
-	cd4N  string
-	cd5N  string
-	cd6N  string
-	cd7N  string
-	cd8N  string
-	cd1   string
-	cd2   string
-	cd3   string
-	cd4   string
-	cd5   string
-	cd6   string
-	cd7   string
-	cd8   string
-
-	cCashFlowN string
-	cCashFlow  string
-	cf1N       string
-	cf2N       string
-	cf3N       string
-	cf4N       string
-	cf5N       string
-	cf6N       string
-	cf7N       string
-	cf8N       string
-	cf1        string
-	cf2        string
-	cf3        string
-	cf4        string
-	cf5        string
-	cf6        string
-	cf7        string
-	cf8        string
-	cf9        string
-	cf10       string
+	CstockID        string `json:"cStockID" bson:"cStockID"`
+	CstockName      string `json:"cStockName" bson:"cStockName"`
+	CnewestSeason   string `json:"cNewestSeason" bson:"cNewestSeason"`
+	CnewestRev      string `json:"cNewestRev" bson:"cNewestRev"`
+	Csign1          string `json:"cSign1" bson:"cSign1"`
+	Csign2          string `json:"cSign2" bson:"cSign2"`
+	Csign3          string `json:"cSign3" bson:"cSign3"`
+	Csign4          string `json:"cSign4" bson:"cSign4"`
+	Csign5          string `json:"cSign5" bson:"cSign5"`
+	Csign6          string `json:"cSign6" bson:"cSign6"`
+	CaverageScore   string `json:"cAverageScore" bson:"cAverageScore"`
+	ClossGain       string `json:"cLossGain" bson:"cLossGain"`
+	CreateDate      string `json:"CreateDate" bson:"CreateDate"`
+	CrevN           string `json:"cRevN" bson:"cRevN"`
+	Crev            string `json:"cRev" bson:"cRev"`
+	Ca1N            string `json:"ca1N" bson:"ca1N"`
+	Ca2N            string `json:"ca2N" bson:"ca2N"`
+	Ca3N            string `json:"ca3N" bson:"ca3N"`
+	Ca4N            string `json:"ca4N" bson:"ca4N"`
+	Ca5N            string `json:"ca5N" bson:"ca5N"`
+	Ca6N            string `json:"ca6N" bson:"ca6N"`
+	Ca7N            string `json:"ca7N" bson:"ca7N"`
+	Cna1            string
+	Cna2            string
+	Cna3            string
+	Cna4            string
+	Cna5            string
+	Cna6            string
+	Cna7            string
+	Cna9            string
+	Cna10           string
+	CnewestRevMonth string
+	CluX            string
+	CnluXMoM        string
+	CProfitN        string
+	CProfit         string
+	Cb1N            string
+	Cb2N            string
+	Cb3N            string
+	Cb4N            string
+	Cb5N            string
+	Cb6N            string
+	Cb7N            string
+	Cb8N            string
+	Cb1             string
+	Cb2             string
+	Cb3             string
+	Cb4             string
+	Cb5             string
+	Cb6             string
+	Cb7             string
+	Cb8             string
+	Cb9             string
+	Cb10            string
+	Cb10P           string
+	CInvTON         string
+	CInvTO          string
+	Ce1N            string
+	Ce2N            string
+	Ce3N            string
+	Ce4N            string
+	Ce5N            string
+	Ce6N            string
+	Ce7N            string
+	Ce8N            string
+	Ce1             string
+	Ce2             string
+	Ce3             string
+	Ce4             string
+	Ce5             string
+	Ce6             string
+	Ce7             string
+	Ce8             string
+	CnewestFinQ     string
+	CNetIncomeN     string
+	CNetIncome      string
+	Cc1N            string
+	Cc2N            string
+	Cc3N            string
+	Cc4N            string
+	Cc5N            string
+	Cc6N            string
+	Cc7N            string
+	Cc8N            string
+	Cc1             string
+	Cc2             string
+	Cc3             string
+	Cc4             string
+	Cc5             string
+	Cc6             string
+	Cc7             string
+	Cc8             string
+	Cc9             string
+	Cc10            string
+	Cc11            string
+	Cpc9            string
+	Cpc10           string
+	Cpc11           string
+	CEPSN           string
+	CEPS            string
+	CD1N            string
+	CD2N            string
+	CD3N            string
+	CD4N            string
+	CD5N            string
+	CD6N            string
+	CD7N            string
+	CD8N            string
+	CD1             string
+	CD2             string
+	CD3             string
+	CD4             string
+	CD5             string
+	CD6             string
+	CD7             string
+	CD8             string
+	CCashFlowN      string
+	CCashFlow       string
+	Cf1N            string
+	Cf2N            string
+	Cf3N            string
+	Cf4N            string
+	Cf5N            string
+	Cf6N            string
+	Cf7N            string
+	Cf8N            string
+	Cf1             string
+	Cf2             string
+	Cf3             string
+	Cf4             string
+	Cf5             string
+	Cf6             string
+	Cf7             string
+	Cf8             string
+	Cf9             string
+	Cf10            string
 }
 
 var collection *mongo.Collection
@@ -197,7 +184,7 @@ func main() {
 func readItem(c *gin.Context) {
 	name := c.Param("name")
 
-	var item Item1
+	var item Item
 	err := collection.FindOne(context.Background(), bson.M{"cStockID": name}).Decode(&item)
 	if err != nil {
 		c.JSON(404, gin.H{"error": "Item not found"})
